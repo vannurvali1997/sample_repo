d0bad84998c0e3db18670cfa959d9aa3872616cc
Jeevankumar37 <jeevankumar.karupothula@gmail.com>
2023-10-27 12:03:34 +0530

    this is the shellscript functions file

---
+++ shellscript.sh
@@ -0,0 +1,33 @@
+add(){
+
+echo " this line comes from add function"
+date
+
+}
+
+forloop(){
+
+echo"printing the alpha bets" 
+
+for letter in {A..Z} {a..z}; do
+
+  echo  "$letter"               
+done
+
+}
+
+sub(){
+
+echo "this line is came from sub function"
+du
+
+}
+
+
+multiply(){
+
+echo "this line has came from multiply"
+df
+
+}
+
